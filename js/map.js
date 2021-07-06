67db78d1f734116b48629f456d661e02915739c6
Tanya Suglobova <tany.suglobova@gmail.com>
2021-07-06 23:40:50 +1200

    Исправили файл с картой и нашли работу с сетью

--- js/map.js
+++ js/map.js
@@ -1,10 +1,18 @@
+import {toggleFormsCondition, addressField} from './form.js';
+import {createNewCard} from './card.js';
 import {initialCoordinates} from './data.js';
 
 const map = L.map('map-canvas')
+  .on('load', () => {
+    setTimeout(() => {
+      toggleFormsCondition(false);
+
+    }, 1000);
+  })
   .setView({
     lat: initialCoordinates.lat,
     lng: initialCoordinates.lng,
-  }, 13);
+  }, 10);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -13,13 +21,53 @@ L.tileLayer(
   },
 ).addTo(map);
 
-const createOfferMarker = () => {
+const mainPinIcon = L.icon({
+  iconUrl: 'img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker({
+  lat: initialCoordinates.lat,
+  lng: initialCoordinates.lng,
+},
+{
+  draggable: true,
+  icon: mainPinIcon,
+},
+);
+mainPinMarker.addTo(map);
+const getCoordinate = (lat, lng) => `широта: ${lat.toFixed(4)}  долгота: ${lng.toFixed(4)}`;
+mainPinMarker.on('drag', (evt) => {
+  const {lat, lng} = evt.target.getLatLng();
+  addressField.value = getCoordinate(lat, lng);
+});
+
+const createOfferMarker = (evt) => {
+  evt.preventDefault();
+  const offerPinIcon = L.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+  const offerPinMarker = L.marker(
+
+    mainPinMarker.getLatLng(),
+    {
+      icon: offerPinIcon,
+    },
+  );
+  offerPinMarker
+    .addTo(map)
+    .bindPopup(createNewCard(),
+      {
+        keepInView: true,
+      },
+    );
 };
 
-
-// const formField =  document.querySelector('.ad-form');
-// formField.addEventListener('submit', (evt) => {
-//   createOfferMarker(evt);
-//   formField.reset();
-// });
-export {createOfferMarker};
+const formField =  document.querySelector('.ad-form');
+formField.addEventListener('submit', (evt) => {
+  createOfferMarker(evt);
+  formField.reset();
+});
